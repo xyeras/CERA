82cb51d6a1797180d2d4c67015dd9ed8a6e7882c
xxxtraa <ryacho@gmail.com>
2018-05-09 23:04:13 -0700

    Add files via upload

--- include/enms2.h
+++ include/enms2.h
@@ -1,26 +1,34 @@
 #ifndef ENMS2_H
 #define ENMS2_H
-
+#include <textureLoader.h>
 #include <gl/gl.h>
 #include <timer.h>
+
+typedef struct
+{
+    float x;
+    float y;
+    float sizes;
+}loc;
 
 class enms2
 {
     public:
         enms2();
         virtual ~enms2();
-
+        void initAttack(int);
         void drawEnemy();     // draw objects
         void placeEnemy(float, float,float); // place objects
         void actions(); //movement
-
+        void drawAttack();
         bool isEnemyLive= true; //status of the objects
         GLuint EnemyTex;      // handle Texture
+        GLuint arrTex;
+        loc arrowLoc;
         int EnemyLife;
         float xPos;             //placing objects on x
         float yPos;             //placing objects on y
-        float zPos;             //placing objects on z
-
+        float zPos;
         float xSize;            // for scaling x
         float ySize;            // for scaling y
 
